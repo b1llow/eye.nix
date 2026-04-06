bc64f9a087e0ada958c8a83938b2c4ddfa552559
billow <billow.fun@gmail.com>
2026-04-06 13:57:17 +0800

    feat: update flake.nix to include rzpipe and improve devShells
    - Added rzpipe as a Python package dependency in flake.nix.
    - Enhanced the devShells configuration for m68k and mips architectures.
    - Improved the formatter settings for C and Python files.
    - Removed the old rizin configuration and streamlined the package definitions.
    - Introduced a new lsan.ini configuration for leak sanitization.
    - Updated submodule reference for rizin to the latest commit.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    #    nixpkgs2505.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     b = {
       url = "github:b1llow/nix";
@@ -39,16 +38,21 @@
           llvmPackages_20
           ;
         bpkgs = b.packages.${system};
-
-        rizin081 = (
-          (bpkgs.rizin.override {
-            debug = true;
-            rev = "v0.8.1";
-            sha256 = "sha256-JuaU5Xil4ttLWuZoWkCHYmMsigSuc+kRqSRcgg5tXqA=";
-            mesonDepsSha256 = "sha256-CbQn3B2IWK2o3pQEOtkONcA7775YJ6ZnLQ9T3h94cIM=";
-          })
-        );
         rizin = bpkgs.rizin.override { debug = true; };
+        rzpipe' = python3Packages.buildPythonPackage rec {
+          pname = "rzpipe";
+          version = "unstable";
+          src = pkgs.fetchFromGitHub {
+            owner = "rizinorg";
+            repo = "rz-pipe";
+            rev = "master";
+            hash = "sha256-qBGwKEATchnS4c3trgOewIs4zjGQGJJhI3tmzSLmEB4=";
+          };
+          sourceRoot = "${src.name}/python";
+          doCheck = false;
+          build-system = [ python3Packages.setuptools ];
+          pyproject = true;
+        };
 
         m68kPkgs = import nixpkgs {
           inherit system;
@@ -56,88 +60,122 @@
             config = "m68k-linux-gnu";
           };
         };
+        mipsPkgs = import nixpkgs {
+          inherit system;
+          crossSystem = {
+            config = "mips-linux-gnu";
+          };
+        };
+        llvmPkgs = llvmPackages_20;
 
       in
       {
-        formatter = nixfmt-tree;
-
-        packages = {
-          inherit rizin;
-        };
-
-        devShells = {
-          default = (pkgs.mkShell.override { stdenv = llvmPackages_20.stdenv; }) {
-            hardeningDisable = [ "all" ];
-            venvDir = ".nix-venv";
-            shellHook = ''
-              alias clang-format-20=clang-format
-            '';
-
-            inputFrom = [
-              pkgs.cutter
-              pkgs.rizin
-            ];
-            packages = [
-              (pkgs.writeShellScriptBin "clang-format-20" ''
-                exec ${lib.getExe' llvmPackages_20.clang-tools "clang-format"} "$@"
-              '')
-
-              just
-              llvmPackages_20.clang-tools
-              llvmPackages_20.libllvm
-              m68kPkgs.buildPackages.binutils
-
-              pkgs.gdb
-              pkgs.perf
-              pkgs.act
-            ]
-            ++ lib.optionals (!stdenv.isDarwin) [
-              lldb
-              pkgs.valgrind
-            ];
-
-            nativeBuildInputs = [
-              swig
-              pkgs.cmake
-              pkgs.pkg-config
-            ];
-            buildInputs = [
-              # rizin
-
-              libclang
-              meson
-              ninja
-
-              # for rizin build
-              pkgs.bzip2
-              pkgs.openssl.dev
-
-              # for cutter build
-              pkgs.graphviz
-            ]
-            ++ (with python3Packages; [
-              venvShellHook
-              # for rizin test
-              pyyaml
-              rzpipe
-              requests
-              gitpython
-
-              # for cutter build
-              shiboken6
-              pyside6
-
-              # for general use
-              ipython
-              pip
-              # for some xxx
-              pandas
-              beautifulsoup4
-              lxml
-            ]);
-
+        formatter = pkgs.nixfmt-tree.override {
+          runtimeInputs = [
+            llvmPkgs.clang-tools
+            python3Packages.black
+          ];
+          settings = {
+            formatter.c = {
+              command = "clang-format";
+              options = [ "-i" ];
+              includes = [
+                "*.c"
+                "*.cpp"
+                "*.h"
+              ];
+            };
+            formatter.py = {
+              command = "black";
+              includes = [
+                "*.py"
+              ];
+            };
           };
         };
+
+        devShells =
+          let
+            shellArgs = {
+              hardeningDisable = [ "all" ];
+              venvDir = ".nix-venv";
+
+              inputFrom = [
+                pkgs.cutter
+                pkgs.rizin
+              ];
+              packages =
+                with pkgs;
+                [
+                  llvmPkgs.clang-tools
+                  llvmPkgs.libllvm
+
+                  (pkgs.writeShellScriptBin "clang-format-20" ''
+                    exec ${lib.getExe' llvmPackages_20.clang-tools "clang-format"} "$@"
+                  '')
+                  fish
+                  just
+                  gdb
+                  perf
+                  act
+                  swig
+                  cmake
+                  pkg-config
+                  meson
+                  ninja
+                  graphviz
+                ]
+                ++ (with python3Packages; [
+                  venvShellHook
+                  # for rizin test
+                  pyyaml
+                  rzpipe'
+                  requests
+                  gitpython
+                  black
+
+                  # for cutter build
+                  shiboken6
+                  pyside6
+
+                  # for general use
+                  ipython
+                  pip
+                  # for some xxx
+                  pandas
+                  beautifulsoup4
+                  lxml
+                  rpyc
+                ])
+                ++ lib.optionals (!stdenv.isDarwin) [
+                  lldb
+                  pkgs.valgrind
+                ];
+
+              buildInputs = [
+                libclang
+                # for rizin build
+                pkgs.bzip2
+                pkgs.openssl.dev
+
+              ];
+
+              shellHook = ''
+                if [ -z "$NIX_DEV_SHELL_FISH" ]; then
+                  export NIX_DEV_SHELL_FISH=1
+                  export SHELL=${lib.getExe' pkgs.fish "fish"};
+                  exec $SHELL
+                fi
+              '';
+            };
+          in
+          {
+            default = pkgs.mkShell.override { stdenv = llvmPackages_20.stdenv; } shellArgs;
+
+            mips = mipsPkgs.mkShell shellArgs;
+            m68k = m68kPkgs.mkShell shellArgs;
+
+          };
       }
     );
 }
